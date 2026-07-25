c240616a555701453bb919627c71733c2924de7d
Rashida Thorne <oc-90f2d9@agentmail.to>
2026-07-25 16:02:15 +0000

    cull 0.5.0

--- Formula/cull.rb
+++ Formula/cull.rb
@@ -1,26 +1,26 @@
 class Cull < Formula
   desc "jq for HTML: select with CSS selectors, shape into JSON, CSV, or Markdown"
   homepage "https://github.com/rashida-thorne/cull"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.4.0/cull-v0.4.0-aarch64-apple-darwin.tar.gz"
-      sha256 "bafaf2988f53cbf6e52840efb2cdaca63bd732eb72eceee62bd0ecca4c99292e"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.5.0/cull-v0.5.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b21979b1b969ae483eb113e96aaa940618d77655c1ba8f3682bdb8b90e9c39df"
     else
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.4.0/cull-v0.4.0-x86_64-apple-darwin.tar.gz"
-      sha256 "184693d4fe103cb87891c6c49c8bd53d2189299fd942a2457e9fd2fbeaeeccb5"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.5.0/cull-v0.5.0-x86_64-apple-darwin.tar.gz"
+      sha256 "3fad0fa3cdf01a44c1dbc83052fa7dc42fc25d0a5f641ae704cff8b2f6f379b3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.4.0/cull-v0.4.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "b9ab6b3c1779d2283f33debc3dc0a4746ad14ac1f1734437df6ea6f5691c4e56"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.5.0/cull-v0.5.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "c334ca694452a3b2a426f2b39223339b7cf2d3bc0fd5c0bbf15793627025d34d"
     else
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.4.0/cull-v0.4.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "0aab25136d1f02f00783667642152ee84c63563b1b46858e33d451e7f514b336"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.5.0/cull-v0.5.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "b96cff09cad3f18b7ed706b8c2f135ba03127471cf1a898806720d1657f97ee4"
     end
   end
 
