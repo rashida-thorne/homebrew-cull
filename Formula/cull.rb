779dab5c0903f8db2ba3af48809c7790d96e45d3
Rashida Thorne <oc-90f2d9@agentmail.to>
2026-07-26 23:27:53 +0000

    cull 0.12.0

--- Formula/cull.rb
+++ Formula/cull.rb
@@ -1,26 +1,26 @@
 class Cull < Formula
   desc "jq for HTML: select with CSS selectors, shape into JSON, CSV, or Markdown"
   homepage "https://github.com/rashida-thorne/cull"
-  version "0.11.2"
+  version "0.12.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.2/cull-v0.11.2-aarch64-apple-darwin.tar.gz"
-      sha256 "01057566cf3f3363b6d984e04edcdba4c77d470153e070382400d7aa6aadc849"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.12.0/cull-v0.12.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b68e17c1a538f150d94dc7fc2852afdf8c5465bc6f9ffc667a17739d3a008e30"
     else
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.2/cull-v0.11.2-x86_64-apple-darwin.tar.gz"
-      sha256 "68b1335d2c29c75980ba658cb999e05ff90d401f57538a6f7931c815f387df4a"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.12.0/cull-v0.12.0-x86_64-apple-darwin.tar.gz"
+      sha256 "4cd53b5da9da69739a4576038d3877c6e48ebd5d6a2da04f06c48b5c773af54c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.2/cull-v0.11.2-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "ed598c504ed5a1e229f63c76cdcbcbc7c1942d1d67ecd4aa2046b5b3f6570e1b"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.12.0/cull-v0.12.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "1032f49da9fabed2f38cdb95ec5e6260cd9bf6880ab48efec3fdd5714e2a0527"
     else
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.2/cull-v0.11.2-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "a2628b9385d47abcc4c4ff6fb538748f8565888a7eb4f7db99a15bffd56ec91e"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.12.0/cull-v0.12.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "ca4673f68cdcbdaaacce25a785aea5f1cbbcd5c4d003dd5938bc5503dbe201bf"
     end
   end
 
