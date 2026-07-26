1e35e509fb12ffdcee35991782ac7470874db788
Rashida Thorne <oc-90f2d9@agentmail.to>
2026-07-26 21:31:07 +0000

    cull 0.11.1 (interactive selector mode)

--- Formula/cull.rb
+++ Formula/cull.rb
@@ -1,26 +1,26 @@
 class Cull < Formula
   desc "jq for HTML: select with CSS selectors, shape into JSON, CSV, or Markdown"
   homepage "https://github.com/rashida-thorne/cull"
-  version "0.10.0"
+  version "0.11.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.10.0/cull-v0.10.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ee40db79ccd59fb9536b47f2a8179cd61453cbda9a432a736abd48cf207df6a7"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.1/cull-v0.11.1-aarch64-apple-darwin.tar.gz"
+      sha256 "716b6e70de0c98b3b3bce596785ffc487398e693f008285447c7f755655e9b56"
     else
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.10.0/cull-v0.10.0-x86_64-apple-darwin.tar.gz"
-      sha256 "9643f9a53ce62cf781db592a6d29e8008491e8b75be4d117c866067b4d7a42a3"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.1/cull-v0.11.1-x86_64-apple-darwin.tar.gz"
+      sha256 "77c5fb53dbdb544aef5fc7c2543384f0825e2ab631abee12321d88dcc80915ad"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.10.0/cull-v0.10.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "08b2935d034ec901e7d949cd1ade9d797d4924fa0b9b0502c989406194da82cd"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.1/cull-v0.11.1-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "ca3e73927e46e90e03b043d7e7278147393975e35f898c866f068bd79530e4bc"
     else
-      url "https://github.com/rashida-thorne/cull/releases/download/v0.10.0/cull-v0.10.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "9d674008ea8fabd99f7978a69cffc0c9cd5bc6e7533564944ab1a175e8db7f1b"
+      url "https://github.com/rashida-thorne/cull/releases/download/v0.11.1/cull-v0.11.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "8fe8ed7aae3b00d08b413ae4eec7fe653b7e3c300c4e1e01478549537ec35b4f"
     end
   end
 
